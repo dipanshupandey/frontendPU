d188fa5811146ef58111bde61e26814889c2c5fa
Dipanshu <dipanshupandey4488@gmail.com>
2026-04-19 13:46:43 +0530

    fixed cors

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
 
 const LoginForm = () => {
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+    const [email,setEmail]=useState("bhch!23@gmail.com");
+    const [password,setPassword]=useState("dipanshu@123");
+    const handleLoginClick=async ()=>{
+        try {
+            const res=await axios.post('http://localhost:5050/user/login',{
+            email:"bhch!23@gmail.com",
+            password:"dipanshu@123"
+        },{withCredentials:true});
+        } catch (error) {
+         console.log(error);
+         alert("Invalid credentials");  
+        }
+    }
     return (
         <div className="">
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
@@ -14,7 +26,7 @@ const LoginForm = () => {
                 <label className="label">Password {password}</label>
                 <input type="password" className="input" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
 
-                <button className="btn btn-neutral mt-4">Login</button>
+                <button className="btn btn-neutral mt-4" onClick={()=>handleLoginClick()}>Login</button>
             </fieldset>
         </div>
     )

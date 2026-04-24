28e29a9d46b2e36531c78ded0143f34f2572e75c
Dipanshu <dipanshupandey4488@gmail.com>
2026-04-24 11:00:59 +0530

    feat(auth): implement secure logout with cookie invalidation and client side state management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,27 @@
-const Home=()=>{
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { logout } from "../utils/userSlice";
+const Home = () => {
+    const dispatch=useDispatch();
+    async function handleLogout() {
+        try {
+            console.log("logout");
+            await axios.post(`${BASE_URL}user/logout`, {}, {
+                withCredentials: true,
+            });
+            dispatch(logout());
+        } catch (error) {
+            console.log(error);
+            dispatch(logout());
+        }
+
+    }
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
     )
 }
 

239a491334e02e4e02966738747e97f995d4d7a5
Dipanshu Pandey <dipanshupandey4488@gmail.com>
2026-05-14 08:51:47 +0530

    feat: implementing chat ui

--- src/components/Chat/ChatSidebar.jsx
+++ src/components/Chat/ChatSidebar.jsx
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
 import {  useSelector } from 'react-redux';
-import Connection from '../Connection';
+
+import ChatSidebarItem from './ChatSidebarItems';
 
 const ChatSidebar=()=>{
     const connections=useSelector((store)=>store.connections);
-    console.log(connections);
-    return <div className="bg-amber-950 w-[25%]">
-        {connections.map((item)=>{
-            return <Connection connectionData={item} variant="connections"/>
+    
+    return <div className=" w-[25%] h-screen overflow-y-auto">
+        {connections.map((item,index)=>{
+            return <ChatSidebarItem connectionData={item} key={index}/>
         })}
+       
+         {connections.map((item,index)=>{
+            return <ChatSidebarItem connectionData={item} key={index}/>
+        })}
+        
     </div>
 }
 
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import ChatSidebar from "../components/Chat/ChatSidebar";
 import ChatWindow from "../components/Chat/ChatWindow";
 
 const Chat=()=>{
-    return <div className="flex">
+    return <div className="flex ">
     <ChatSidebar/>
     <ChatWindow/>
 </div>
